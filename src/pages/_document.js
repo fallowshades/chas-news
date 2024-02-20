632cd26820baabe6a991dad3c27bb6deb756702c
ahh01 <145448378+ahh01@users.noreply.github.com>
2024-02-20 15:41:52 +0100

    flytt till comp

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import Artikelsidor from "./Artikelsidor";
+import Artikelsidor from "../components/Artikelsidor";
 export default function Document() {
   return (
     <Html lang="en">
