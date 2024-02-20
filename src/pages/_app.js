cbcaf2eb20e220ef9df617ef9ffddb81cb65346d
julialack <145348380+julialack@users.noreply.github.com>
2024-02-20 14:14:14 +0100

    uppdate app.js import sportsnews

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
+import React from "react";
+import SportsNews from "./components/sportsnews";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+const App = () => {
+  return (
+    <div>
+      {/* Annan innehåll och komponenter */}
+      <SportsNews />
+    </div>
+  );
+};
+
+export default App;
