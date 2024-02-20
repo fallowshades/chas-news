cd5236df91083b0bf53b1fc3f22059a8c1b8d069
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-20 15:15:46 +0100

    navigation-Bar start

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <header class="bg-blue-400">
+        <h1>Navigation-Bar-Here</h1>
+      </header>
+      <Component {...pageProps} />
+    </>
+  );
 }
