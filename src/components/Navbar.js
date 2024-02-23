88dabaeb89ffd0c804ca0e07ba70ef65f8b14686
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-23 15:56:11 +0100

    Dropdown "Categories" in the Nab-bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,19 +9,21 @@ const Navbar = () => {
       <nav class="p-4 text-white">
         <ul class="flex gap-14 text-xl list-none m-0 p-0">
           <li class={textStyle}>
-            <Link href="/">Last-News</Link>
+            <Link href="/">Latest-News</Link>
           </li>
-          {/*   <li>
+          <li>
             <Link href="#">Categories</Link>
-          </li> */}
-          <li class={textStyle}>
-            <Link href="/categories/international">International-News</Link>
-          </li>
-          <li class={textStyle}>
-            <Link href="/categories/sports">Sport-News</Link>
-          </li>
-          <li class={textStyle}>
-            <Link href="/categories/gameNews">Games-News</Link>
+            <ul className="drop-down">
+              <li class={textStyle}>
+                <Link href="/categories/international">International-News</Link>
+              </li>
+              <li class={textStyle}>
+                <Link href="/categories/sports">Sport-News</Link>
+              </li>
+              <li class={textStyle}>
+                <Link href="/categories/gameNews">Games-News</Link>
+              </li>
+            </ul>
           </li>
           <li class={textStyle}>
             <Link href="/bookmarks2">Bookmarks</Link>
