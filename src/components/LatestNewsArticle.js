0787e70896c264d90f7076b21519ddd452cc5be4
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-23 17:45:37 +0100

    Showing the latest News on the page

--- src/components/LatestNewsArticle.js
+++ src/components/LatestNewsArticle.js
@@ -4,9 +4,13 @@ export default function LatestNewsArticle(props) {
   const title = props.articleObj.title;
   const description = props.articleObj.description;
   const articleImg = props.articleObj.image_url;
+  const realizedDate = props.articleObj.pubDate;
   return (
-    <>
-      <h1>{title}</h1>
-    </>
+    <div class="flex flex-col py-4 border-black items-center">
+      <div class="block text-xl">{realizedDate}</div>
+      <div class="block text-2xl">{title}</div>
+      <img src={articleImg} />
+      <div class="block text-2xl">{description}</div>
+    </div>
   );
 }
