bff9bfaac7f66dee44abda32b8efb6e536d82f5b
AlejandroSZIOSA <ggato33@gmail.com>
2024-02-23 13:43:08 +0100

    Fixing Nav-Bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,25 +5,25 @@ const textStyle = "hover:text-amber-400"; //Tailwind style
 //TODO:FIX CATEGORIES , SUB-MENU
 const Navbar = () => {
   return (
-    <div className="flex flex-row w-full bg-[#343434] items-center justify-around">
-      <nav className="p-4 text-white">
-        <ul className="flex gap-14 text-2xl list-none m-0 p-0">
-          <li className={textStyle}>
+    <div class="flex flex-row w-full bg-[#343434] items-center justify-around">
+      <nav class="p-4 text-white">
+        <ul class="flex gap-14 text-xl list-none m-0 p-0">
+          <li class={textStyle}>
             <Link href="/">Last-News</Link>
           </li>
           {/*   <li>
             <Link href="#">Categories</Link>
           </li> */}
-          <li className={textStyle}>
+          <li class={textStyle}>
             <Link href="/categories/international">International-News</Link>
           </li>
-          <li className={textStyle}>
+          <li class={textStyle}>
             <Link href="/categories/sports">Sport-News</Link>
           </li>
-          <li className={textStyle}>
+          <li class={textStyle}>
             <Link href="/categories/gameNews">Games-News</Link>
           </li>
-          <li className={textStyle}>
+          <li class={textStyle}>
             <Link href="/bookmarks2">Bookmarks</Link>
           </li>
         </ul>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import "@/styles/globals.css";
+import "@/styles/navbar.css";
 
 import Layout from "./layoutProvider";
 export default function App({ Component, pageProps }) {
