705f0f4b9769c15721695e8d3c6b337f6026e0b7
fallowShades <fallowshades@gmail.com>
2024-02-24 10:23:27 +0100

    Move international news to navigated file

--- src/pages/categories/international.js
+++ src/pages/categories/international.js
@@ -1,6 +1,48 @@
-import React from "react";
+import Link from 'next/link'
+import NewsCard from '@/components/NewsCard'
 
-//NOTE:THIS PAGE IS "WORKING" AND CAN BE USED IT
-export default function International() {
-  return <div>International</div>;
+/**
+ * const DIN_API_NYCKEL = 'pub_'
+const QUERY_PARAMETERS = '&country=us&category=world'
+export async function GET() {
+  const filePath = path.resolve(
+    process.cwd(),
+    'assets',
+    'international_news.json'
+  )
+  const res = await fetch()
+
+  //`https://newsapi.org/v2/everything?apiKey=${DIN_API_NYCKEL}${QUERY_PARAMETERS}`
+  // const data = await res.json()
+
+  return {
+    props: {
+      news: data,
+    },
+  }
 }
+ */
+
+import { getLocalData } from '@/lib/localdata'
+
+export async function getStaticProps() {
+  const localData = await getLocalData()
+  console.log(localData)
+  return {
+    props: { news: localData },
+  }
+}
+import { v4 as uuidv4 } from 'uuid'
+const International_news = ({ news }) => {
+  console.log(news)
+  return (
+    <div>
+      <h1>Latest international news</h1>
+
+      {news.map((item) => {
+        return <NewsCard key={uuidv4()} newsItem={item} />
+      })}
+    </div>
+  )
+}
+export default International_news
